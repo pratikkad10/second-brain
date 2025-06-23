baac8c8f8c215a709f306118df962e32e4e985d7
pratik kad <kadpratik22@gmail.com>
2025-06-23 21:34:02 +0530

    api routes defined

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,13 @@
 import express from "express"
 import { login, register } from "../controllers/auth.controller";
+import { addcontent, deletecontent, getcontent } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/authMiddleware";
 const userRoutes = express.Router();
 
 userRoutes.post('/signup', register);
 userRoutes.post('/signin', login);
+userRoutes.post('/content',authMiddleware, addcontent);
+userRoutes.get('/content',authMiddleware, getcontent);
+userRoutes.delete('/content',authMiddleware, deletecontent);
 
 export default userRoutes;
