d96e68c3497ca0efd948e8d4d52998eefbcd8958
pratik kad <kadpratik22@gmail.com>
2025-06-25 16:34:40 +0530

    share content routes added

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { login, register } from "../controllers/auth.controller";
-import { addcontent, deletecontent, getcontent } from "../controllers/user.controller";
+import { addcontent, deletecontent, fetchsharedEntry, getcontent, shareContent } from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/authMiddleware";
 const userRoutes = express.Router();
 
@@ -9,5 +9,8 @@ userRoutes.post('/signin', login);
 userRoutes.post('/content',authMiddleware, addcontent);
 userRoutes.get('/content',authMiddleware, getcontent);
 userRoutes.delete('/content',authMiddleware, deletecontent);
+
+userRoutes.post('/share',authMiddleware, shareContent);
+userRoutes.get('/:share', fetchsharedEntry);
 
 export default userRoutes;
