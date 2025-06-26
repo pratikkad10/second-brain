19c2cc4ede1b004ab3c29c6e1bc2ca39ce5a26d1
pratik kad <kadpratik22@gmail.com>
2025-06-26 22:32:54 +0530

    plus icon added

--- client/src/components/icons/PlusIcon.tsx
+++ client/src/components/icons/PlusIcon.tsx
@@ -1,10 +1,10 @@
 import { IconVariants, type IconInterface } from '.'
 
-const PlusIcon = (props:IconInterface) => {
+const PlusIcon = (props: IconInterface) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${IconVariants[props.size]}`}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
 
   )
 }
