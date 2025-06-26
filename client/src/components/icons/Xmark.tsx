bdba832d61d318358cb03b22db28b012ffb13607
pratik kad <kadpratik22@gmail.com>
2025-06-26 22:35:57 +0530

    x mark added ui

---
+++ client/src/components/icons/Xmark.tsx
@@ -0,0 +1,12 @@
+import { IconVariants, type IconInterface } from "."
+
+const Xmark = (props:IconInterface) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${IconVariants[props.size]}`}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
+
+  )
+}
+
+export default Xmark
