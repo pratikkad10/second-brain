0bee0a7a67c8823dd035c7f38478b199b00fd73d
pratik kad <kadpratik22@gmail.com>
2025-06-26 22:34:08 +0530

    sidebar modified

--- client/src/components/ui/Sidebar.tsx
+++ client/src/components/ui/Sidebar.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import BrainLogo from '../icons/BrainLogo'
-import SidebarItems from './sidebarItems'
 import TwitterLogo from '../icons/TwitterLogo'
 import Youtube from '../icons/Youtube'
 import FileLogo from '../icons/FileLogo'
 import LinkIcon from '../icons/LinkIcon'
 import TagsIcon from '../icons/TagsIcon'
+import SidebarItems from './SidebarItems'
 
 const Sidebar = () => {
   return (
-    <div className='left-0 w-[15%] h-screen bg-custom-light-200 shadow p-6'>
+    <div className='left-0 w-[15%] min-h-screen bg-custom-light-200 shadow p-6'>
         <div className='flex items-center gap-2 mb-8'>
             <span><BrainLogo /></span>
             <h1 className='text-2xl font-semibold'>Secondbrain</h1>
